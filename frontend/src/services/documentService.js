2ae1337218c7e5e7f4b4220a946175cd2b57c956
Anabayan M <anabayasathriyanm.ad24@bitsathy.ac.in>
2026-07-13 23:19:43 +0530

    Update frontend API URLs to Render

--- frontend/src/services/documentService.js
+++ frontend/src/services/documentService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/documents';
+const API_URL = 'https://authenticity-validator-academia.onrender.com/api/documents';
 
 // Axios instance to automatically attach the token
 const axiosInstance = axios.create({
