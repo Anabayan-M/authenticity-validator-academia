010d602c7453d553eaf01f6216a5232e4a930855
Anabayan M <anabayasathriyanm.ad24@bitsathy.ac.in>
2026-07-14 13:15:38 +0530

    Update auth API URL to Render

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8080/api/auth';
+const API_URL = 'https://authenticity-validator-academia.onrender.com/api/auth';
 
 const login = async (email, password) => {
     const response = await axios.post(`${API_URL}/login`, { email, password });
